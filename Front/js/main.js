8314f70797298ef50a04df80f54f25d83b96551a
Nurana Rustamova <nuranaar@code.edu.az>
2019-04-11 03:47:42 +0400

    creat project

---
+++ Front/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+    $(".btn-item").click(function (e) {
+        e.preventDefault();
+        $(this).addClass("active");
+        if ($(this).siblings().hasClass("active")) {
+            $(this).siblings().removeClass("active");
+        }
+    });
+
+    $(".menuBtn").click(function (e) { 
+        e.preventDefault();
+        $(".menu").toggleClass("act");
+    });
+});
